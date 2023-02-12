748add234ad6a81b099663843d8b4bc7ea0fc1d5
musawir252 <musawir252@gmail.com>
2023-02-12 08:57:12 +0000

    Completed Casting Lab

--- src/main/java/CastingPrimitive.java
+++ src/main/java/CastingPrimitive.java
@@ -16,6 +16,8 @@ public class CastingPrimitive {
      * @return the int form of d.
      */
     public int castToInt(double d){
-        return 0;
+        int f = (int) d;
+        return f;
+
     }
 }
